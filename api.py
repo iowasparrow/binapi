60f5726d03084480eca9de7aaa9070f24cf6c599
iowasparrow <gsiebrecht@siebrecht.us>
2020-01-10 08:47:01 -0600

    changed

--- api.py
+++ api.py
@@ -6,8 +6,9 @@ from flask import request
 from flask import flash, redirect
 import sqlite3
 from datetime import datetime, timezone, timedelta
+import json
 
-database = '/var/www/html/binweb/binapi/customer.db'
+database = '/var/www/html/binweb/bin_temperature/sensorsData.db'
 #database = 'customer.db'
 app = Flask(__name__)
 
@@ -66,68 +67,127 @@ def download():  # This returns all entries in the database into a dict and then
 #############
 
 
-@app.route('/api/v1.0/tasks', methods=['GET'])
-def get_all(siteid, start_date='1900-01-01', end_date='2050-01-01'):  # This returns all entries in the database into a dict and then converts to json.
+@app.route('/api/getall', methods=['GET'])
+def get_all(start_date='1900-01-01', end_date='2050-01-01'):  # this is for the chart
     conn = sqlite3.connect(database, check_same_thread=False)
     curs = conn.cursor()
     print("start date in getall function: " + start_date)
-    print("end date in getall function: " + end_date) 
-    sql = "SELECT * FROM tbl_data WHERE siteid  = ? AND timestamp >= ? AND timestamp <= ? ORDER BY timestamp DESC"
-    # we have to change site id to a list because when we get to double digits it thinks we are passing in a list of characters.
-    curs.execute(sql, [siteid, start_date, end_date])
-    
+    print("end date in getall function: " + end_date)
+    sql = "SELECT * FROM pidata WHERE timestamp >= ? AND timestamp <= ? ORDER BY timestamp DESC"
+    curs.execute(sql, [start_date, end_date])
     data = curs.fetchall()
     dates = []
-    temps = []
-    # siteids = []
+    airtemps = []
     soiltemps = []
+    cputemps= []
     sensor1 = []
     sensor2 = []
-    # sensor3 = []
-    # sensor4 = []
-    # sensor5 = []
-    # sensor6 = []
+
     for row in reversed(data):
-        #dates.append(row[0])
-        dates.append(datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S.%f').strftime('%m/%d/%Y %H:%M'))
-        temps.append(row[1])
-        #siteids.append(row[2])
-        soiltemps.append(row[3])
-        
-        #sensor1.append(row[4])
-        
-        
-        if row[4] == None:
-            #convert None to null so the chart is happy
-            sensor1.append('null')
-        elif row[4] == 0:
+        dates.append(datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S').strftime('%m/%d/%Y %H:%M'))
+        #topic
+        airtemps.append(row[2])
+        # siteids.append(row[3])
+        soiltemps.append(row[4])
+        #humidity5
+        if row[6] is None or row[6] == 0:
+            # convert None to null so the chart is happy 
+            cputemps.append('null')
+        else:
+            cputemps.append(row[6])
+
+        if row[7] is None or row[7] == 0:
+            # convert None to null so the chart is happy
             sensor1.append('null')
         else:
-            sensor1.append(row[4])
+            sensor1.append(row[7])
 
-        if row[5] == None or row[5] == 0 or row[5] == 185:
-            #convert None to null so the chart is happy
+        if row[8] is None or row[8] == 0 or row[8] == 185:
+            # convert None to null so the chart is happy
             sensor2.append('null')
         else:
-            sensor2.append(row[5])
+            sensor2.append(row[8])
 
-        # sensor3append(row[6])
+        # sensor3.append(row[6])
         # sensor4.append(row[7])
         # sensor5.append(row[8])
         # sensor6.append(row[9])
 
-        dict[row[0]] = {'date': row[0], 'temp': row[1], 'soiltemps':row[3] ,'sensor1': row[4], 'sensor2': row[5]}
+
+#dict[row[0]] = {'date': row[0], 'temp': row[1], 'soiltemps':row[3] ,'sensor1': row[4], 'sensor2': row[5]}
+#return jsonify({'data': dict})
 
     conn.close()
-    #print(dict)
-    return dates, temps, soiltemps, sensor1, sensor2
-    #return jsonify({'data': dict})
+    return dates ,airtemps, soiltemps, cputemps, sensor1, sensor2
+
+@app.route('/api/getjson', methods=['GET'])
+def get_json(start_date='1900-01-01', end_date='2050-01-01'):  # this is for the chart
+    conn = sqlite3.connect(database, check_same_thread=False)
+    curs = conn.cursor()
+    print("start date in getall function: " + start_date)
+    print("end date in getall function: " + end_date)
+    sql = "SELECT * FROM pidata WHERE timestamp >= ? AND timestamp <= ? ORDER BY timestamp DESC"
+    curs.execute(sql, [start_date, end_date])
+    data = curs.fetchall()
+    dates = []
+    airtemps = []
+    soiltemps = []
+    cputemps= []
+    sensor1 = []
+    sensor2 = []
+
+    for row in reversed(data):
+        dates.append(datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S').strftime('%m/%d/%Y %H:%M'))
+        #topic
+        airtemps.append(row[2])
+        # siteids.append(row[3])
+        soiltemps.append(row[4])
+        #humidity5
+        if row[6] is None or row[6] == 0:
+            # convert None to null so the chart is happy 
+            cputemps.append('null')
+        else:
+            cputemps.append(row[6])
+
+        if row[7] is None or row[7] == 0:
+            # convert None to null so the chart is happy
+            sensor1.append('null')
+        else:
+            sensor1.append(row[7])
+
+        if row[8] is None or row[8] == 0 or row[8] == 185:
+            # convert None to null so the chart is happy
+            sensor2.append('null')
+        else:
+            sensor2.append(row[8])
+
+        # sensor3.append(row[6])
+        # sensor4.append(row[7])
+        # sensor5.append(row[8])
+        # sensor6.append(row[9])
+
+
+        dict[row[0]] = {'date': row[0], 'airtemp': row[2], 'soiltemps':row[4] ,'sensor1': row[7], 'sensor2': row[8]}
+    
+    x = json.dumps(dict)
+    json_object = json.loads(x)
+    json_formatted_str = json.dumps(json_object, indent=2)
+    
+    #print(json_formatted_str)
+    #return json_formatted_str
+    
+    
+    return jsonify({'data': dict})
+
+
+
+
 
 
 def get_average():
     conn = sqlite3.connect(database, check_same_thread=False)
     curs = conn.cursor()
-    sql = "select avg(sensor1) from(select sensor1 from tbl_data WHERE sensor1 <> 'None' AND sensor1 <> '0' Order By timestamp desc limit 20)"
+    sql = "select avg(sensor1) from(select sensor1 from pihq WHERE sensor1 <> 'None' AND sensor1 <> '0' Order By timestamp desc limit 20)"
     # we have to change site id in the execute function to a list because when we get to double digits it thinks we are passing in a list of characters.
     curs.execute(sql)
     data = curs.fetchall()
@@ -141,7 +201,7 @@ def check_rapid_rise(current_temp, x):
     curs = conn.cursor()
     temp_week_ago = 0
     for row in curs.execute(
-            "SELECT * FROM tbl_data WHERE timestamp BETWEEN datetime('now', '-8 days') AND datetime('now', '-6 days') LIMIT 1;"):
+            "SELECT * FROM pihq WHERE timestamp BETWEEN datetime('now', '-8 days') AND datetime('now', '-6 days') LIMIT 1;"):
         temp_week_ago = row[x]
     conn.close()
     if temp_week_ago == None:
@@ -158,26 +218,26 @@ def check_rapid_rise(current_temp, x):
         return formatted_temp_difference, temp_week_ago
 
 
-def get_current_data():  # get current values for display on dashboard
+def get_current_data():  # get current values for display on web page
     conn = sqlite3.connect(database, check_same_thread=False)
     curs = conn.cursor()
-    #current_time = []
+    current_time = []
     current_temp = []
     current_soiltemp = []
-    current_sensor1 = []
-    current_sensor2 = []
-    for row in curs.execute("SELECT * FROM tbl_data ORDER BY timestamp DESC LIMIT 1"):
-        #current_time = str(row[0])
-        current_time = row[0]
-        current_temp = row[1]
-        current_soiltemp = row[3]
-        current_sensor1 = row[4]
-        current_sensor2 = row[5]
+    sensor1 = []
+    sensor2 = []
+    for row in curs.execute("SELECT * FROM pidata ORDER BY timestamp DESC LIMIT 1"):
+        current_time = str(row[0])
+        current_temp = row[2]
+        current_soiltemp = row[4]
+        current_sensor1 = row[7]
+        current_sensor2 = row[8]
     conn.close()
-    temp_difference, temp_week_ago = check_rapid_rise(current_temp, 1)
-    temp_difference1, temp_week_ago1 = check_rapid_rise(current_sensor1, 4)
-    temp_difference2, temp_week_ago2 = check_rapid_rise(current_sensor2, 5)
-    #newdate = datetime.strptime(current_time, '%Y-%m-%d %H:%M:%S.%f').strftime('%m/%d/%Y')
+
+    # send current temp and databse row to check for rapid rise
+    temp_difference, temp_week_ago = check_rapid_rise(current_temp, 2)
+    temp_difference1, temp_week_ago1 = check_rapid_rise(current_sensor1, 7)
+    temp_difference2, temp_week_ago2 = check_rapid_rise(current_sensor2, 8)
 
     return current_time, current_temp, temp_difference, temp_week_ago, current_soiltemp, current_sensor1, current_sensor2, temp_week_ago1, temp_week_ago2, temp_difference1, temp_difference2
 
@@ -191,25 +251,25 @@ def linechart():
     siteid=mycookie
     x = "0"
     if not request.values.get("aStartDate") and not request.values.get("aEndDate"): 
-        dates, temps, soiltemps, sensor1, sensor2 = get_all(siteid)    
+        dates, airtemps, soiltemps, cputemps, sensor1, sensor2 = get_all(siteid)    
     if request.values.get("aStartDate") and request.values.get("aEndDate"):
         start_date = request.values.get("aStartDate")
         end_date = request.values.get("aEndDate")
-        dates, temps, soiltemps, sensor1, sensor2 = get_all(siteid, start_date, end_date)
+        dates, airtemps, soiltemps, cputemps, sensor1, sensor2 = get_all(siteid, start_date, end_date)
     if request.values.get("aStartDate") and not request.values.get("aEndDate"):
         start_date = request.values.get("aStartDate")
-        dates, temps, soiltemps, sensor1, sensor2 = get_all(siteid, start_date)
+        dates, airtemps, soiltemps, cputemps, sensor1, sensor2 = get_all(siteid, start_date)
     if request.values.get("aEndDate") and not request.values.get("aStartDate"):
         end_date = request.values.get("aEndDate")
-        dates, temps, soiltemps, sensor1, sensor2 = get_all(siteid, x, end_date)
+        dates, airtemps, soiltemps, cputemps, sensor1, sensor2 = get_all(siteid, x, end_date)
     
     #  print(dict)
     # return jsonify({'data': dict})
     current_time, current_temp, temp_difference, current_soiltemp, temp_week_ago, current_sensor1, current_sensor2, temp_week_ago1, temp_week_ago2, temp_difference1, temp_difference2 = get_current_data()
    
-    shortdate = datetime.strptime(current_time, '%Y-%m-%d %H:%M:%S.%f').strftime('%m/%d/%Y %H:%M')
+    shortdate = datetime.strptime(current_time, '%Y-%m-%d %H:%M:%S').strftime('%m/%d/%Y %H:%M')
     
-    return render_template('index.html', siteid=siteid, temp_week_ago=temp_week_ago, temp_difference=temp_difference, temp_difference1=temp_difference1, temp_difference2=temp_difference2, temps=temps, dates=dates, soiltemps=soiltemps, current_soiltemp=current_soiltemp ,sensor1=sensor1,sensor2=sensor2, current_sensor1=current_sensor1, current_sensor2=current_sensor2, shortdate=shortdate, current_temp=current_temp, temp_week_ago1=temp_week_ago1, temp_week_ago2=temp_week_ago2)
+    return render_template('index.html', siteid=siteid, temp_week_ago=temp_week_ago, temp_difference=temp_difference, temp_difference1=temp_difference1, temp_difference2=temp_difference2, temps=airtemps, dates=dates, soiltemps=soiltemps, current_soiltemp=current_soiltemp ,sensor1=sensor1,sensor2=sensor2, current_sensor1=current_sensor1, current_sensor2=current_sensor2, shortdate=shortdate, current_temp=current_temp, temp_week_ago1=temp_week_ago1, temp_week_ago2=temp_week_ago2)
 
 
 @app.route('/login', methods=['POST', 'GET'])
@@ -253,11 +313,14 @@ def dashboard():
     #  print(dict)
     # return jsonify({'data': dict})
     avg = get_average()
-    current_time, current_temp, temp_difference, current_soiltemp, temp_week_ago, current_sensor1, current_sensor2, temp_week_ago1, temp_week_ago2, temp_difference1, temp_difference2 = get_current_data()
 
-    dates, temps, soiltemps, sensor1, sensor2 = get_all(siteid)
+    current_time, current_temp, temp_difference, temp_week_ago, current_soiltemp, current_sensor1, current_sensor2, temp_week_ago1, temp_week_ago2, temp_difference1, temp_difference2 = get_current_data()
+    
+    #print("current soiltemp- " + str(current_soiltemp))
 
-    return render_template('dashboard.html', siteid=siteid, avg=avg, temp_week_ago=temp_week_ago, mycookie=mycookie, temp_difference=temp_difference,temps=temps, dates=dates, soiltemps=soiltemps ,sensor1=sensor1, current_sensor1=current_sensor1, current_sensor2=current_sensor2, sensor2=sensor2, current_time=current_time, current_temp=current_temp, current_soiltemp=current_soiltemp, temp_week_ago1=temp_week_ago1, temp_week_ago2=temp_week_ago2 )
+    dates, airtemps, soiltemps, cputemps, sensor1, sensor2 = get_all(siteid)
+
+    return render_template('dashboard.html', siteid=siteid, avg=avg, temp_week_ago=temp_week_ago, mycookie=mycookie, temp_difference=temp_difference,temps=airtemps, dates=dates, soiltemps=soiltemps ,sensor1=sensor1, current_sensor1=current_sensor1, current_sensor2=current_sensor2, sensor2=sensor2, current_time=current_time, current_temp=current_temp, current_soiltemp=current_soiltemp, temp_week_ago1=temp_week_ago1, temp_week_ago2=temp_week_ago2 )
 
 
 @app.route('/delete-cookie/')
