8a769db5a3a59f8cddc7aa361bc7b98d21c8d4df
iowasparrow <grant@siebrecht.us>
2020-01-10 16:50:23 +0000

    changed

--- api.py
+++ api.py
@@ -312,13 +312,15 @@ def dashboard():
     siteid = mycookie
     #  print(dict)
     # return jsonify({'data': dict})
-    avg = get_average()
+    #avg = get_average()
 
     current_time, current_temp, temp_difference, temp_week_ago, current_soiltemp, current_sensor1, current_sensor2, temp_week_ago1, temp_week_ago2, temp_difference1, temp_difference2 = get_current_data()
     
     #print("current soiltemp- " + str(current_soiltemp))
 
     dates, airtemps, soiltemps, cputemps, sensor1, sensor2 = get_all(siteid)
+
+    avg = 0
 
     return render_template('dashboard.html', siteid=siteid, avg=avg, temp_week_ago=temp_week_ago, mycookie=mycookie, temp_difference=temp_difference,temps=airtemps, dates=dates, soiltemps=soiltemps ,sensor1=sensor1, current_sensor1=current_sensor1, current_sensor2=current_sensor2, sensor2=sensor2, current_time=current_time, current_temp=current_temp, current_soiltemp=current_soiltemp, temp_week_ago1=temp_week_ago1, temp_week_ago2=temp_week_ago2 )
 
